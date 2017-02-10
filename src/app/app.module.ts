aab9f0704737f6f0eaa7af16d02b2d9ad6d2e60c
Johan Roug <joro@sdc.dk>
2017-02-11 00:06:14 +0100

    added wine REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { AppComponent }             from './app.component';
 
 import { MywinesComponent }         from "./components/mywines/mywines.component";
 import { MywinesOverviewComponent } from "./components/mywines/overview/overview.component";
+import {MywineWineService} from "./components/mywines/wine-com/mywines-wine.service";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule ],
   declarations: [
     AppComponent,
     MywinesComponent,
     MywinesOverviewComponent ], // Components
   bootstrap:    [ AppComponent ],
-  providers:    [ ] // Services
+  providers:    [ MywineWineService ] // Services
 })
 export class AppModule { }
--- src/app/components/mywines/overview/overview.component.ts
+++ src/app/components/mywines/overview/overview.component.ts
@@ -1,19 +1,29 @@
 import {Component} from "@angular/core";
+import {MywineWineService} from "../wine-com/mywines-wine.service";
 
 @Component({
   selector: "mywines-overview",
-  template: `
+  template: `    
     <ul>
       <li *ngFor="let item of winelist">
-         {{item.name}}
+         {{item.name}}         
       </li>
     </ul>    
+    
+    <pre>{{wineCom | json}}</pre>
   `
 })
 export class MywinesOverviewComponent {
   public winelist: any;
+  public wineCom;
 
-  constructor() {
+  constructor(private _mywineWineService: MywineWineService) {
+    this._mywineWineService.getData()
+      .subscribe(
+        (data) => {
+          this.wineCom = data;
+        }
+      );
     this.winelist = [
       {
         name: "johan2",
