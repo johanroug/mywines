4a19177f5a90b11b9179022e74480a8bda3a7919
Johan Roug <joro@sdc.dk>
2017-02-12 17:00:11 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <mywines></mywines>
+    <div class="container">
+      <mywines></mywines>
+    </div>
   `
 })
 export class AppComponent {}
--- src/app/components/mywines/mywines.component.ts
+++ src/app/components/mywines/mywines.component.ts
@@ -3,7 +3,11 @@ import {Component} from "@angular/core";
 @Component({
   selector: "mywines",
   template: `
-    <mywines-overview></mywines-overview>  
+    <div class="row">
+      <div class="col-xs-12 col-md-6">
+        <mywines-overview></mywines-overview>
+      </div>
+    </div>
   `
 })
 export class MywinesComponent {
